49c216b28f0470c601f4381451da07f69ecb6c16
Hamza <touhs.hamza@gmail.com>
2020-04-26 18:49:31 -0700

    Heartbeat + Partial results

--- DroneSwarmManagement/src/HeartBeatReceiving.java
+++ DroneSwarmManagement/src/HeartBeatReceiving.java
@@ -1,71 +1,75 @@
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
 
-public class DataCollection implements Runnable {
-    public DataCollection() {
+public class HeartBeatReceiving implements Runnable {
+    public HeartBeatReceiving() {
     }
-
+    
     public void run() {
         while(true){
-            prototype.textArea5.append("Done[3]: Assemble Data From #546WD\n");
+            prototype.textArea7.append("Done[3]: Ack from #546WD\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[2]: Assemble Data From #632WD\n");
+            Scanner sc= new Scanner(System.in); //System.in is a standard input stream.
+            System.out.print("Enter first number- ");
+            int a= sc.nextInt();
+            prototype.textArea7.append("Done[2]: Ack from #632WD\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[4]: Assemble Data From #723WD\n");
+            prototype.textArea7.append("Done[4]: Ack from #723WD\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[3]: Assemble Data From #123WD\n");
+            prototype.textArea7.append("Done[3]: Ack from #123WD\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[2]: Assemble Data From #746WD\n");
+            prototype.textArea7.append("Done[2]: Ack from #746WD\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[4]: Assemble Data From #867WD\n");
+            prototype.textArea7.append("Done[4]: Ack from #867WD\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[3]: Assemble Data From #643WD\n");
+            prototype.textArea7.append("Done[3]: Ack from #643WD\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[2]: Assemble Data From #132WD\n");
+            prototype.textArea7.append("Done[2]: Ack from #132WD\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[3]: Assemble Data From #364WD\n");
+            prototype.textArea7.append("Done[3]: Ack from #364WD\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[4]: Assemble Data From #532WD\n");
+            prototype.textArea7.append("Done[4]: Ack from #532WD\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
--- DroneSwarmManagement/src/HeartBeatSending.java
+++ DroneSwarmManagement/src/HeartBeatSending.java
@@ -1,71 +1,75 @@
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
 
-public class DataCollection implements Runnable {
-    public DataCollection() {
+public class HeartBeatSending implements Runnable {
+    public HeartBeatSending() {
     }
-
+    
     public void run() {
         while(true){
-            prototype.textArea5.append("Done[3]: Assemble Data From #546WD\n");
+            prototype.textArea9.append("Done[546WD]: Send heartbeat to MD[3]\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[2]: Assemble Data From #632WD\n");
+            Scanner sc= new Scanner(System.in); //System.in is a standard input stream.
+            System.out.print("Enter first number- ");
+            int a= sc.nextInt();
+            prototype.textArea9.append("Done[632WD]: Send heartbeat to MD[2]\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[4]: Assemble Data From #723WD\n");
+            prototype.textArea9.append("Done[723WD]: Send heartbeat to MD[1]\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[3]: Assemble Data From #123WD\n");
+            prototype.textArea9.append("Done[123WD]: Send heartbeat to MD[1]\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[2]: Assemble Data From #746WD\n");
+            prototype.textArea9.append("Done[7234WD]: Send heartbeat to MD[2]\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[4]: Assemble Data From #867WD\n");
+            prototype.textArea9.append("Done[867WD]: Send heartbeat to MD[4]\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[3]: Assemble Data From #643WD\n");
+            prototype.textArea9.append("Done[643WD]: Send heartbeat to MD[3]\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[2]: Assemble Data From #132WD\n");
+            prototype.textArea9.append("Done[132WD]: Send heartbeat to MD[2]\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[3]: Assemble Data From #364WD\n");
+            prototype.textArea9.append("Done[364WD]: Send heartbeat to MD[1]\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
                 Logger.getLogger(prototype.class.getName()).log(Level.SEVERE, null, ex);
             }
-            prototype.textArea5.append("Done[4]: Assemble Data From #532WD\n");
+            prototype.textArea9.append("Done[532WD]: Send heartbeat to MD[4]\n");
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException ex) {
